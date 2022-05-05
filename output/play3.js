94cd5c6853e188689f998e4ab4185ddfba76a452
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2022-05-05 17:57:38 +0300

    Add More Conditional Example

---
+++ output/play3.js
@@ -0,0 +1,18 @@
+
+"use strict";/*Compiled using nearley and moojs*/var _u=Math.imul,_b=Math.abs,_p=Math.pow,_s=Math.sqrt;function print(){var n;(n=console).log.apply(n,arguments)}function $if(n,r,t){(n?r:t)()}function gt(n,r){return r<n}var add=function(){for(var n=arguments.length,r=Array(n),t=0;t<n;t++)r[t]=arguments[t];return r.reduce(function(n,r){return n+r},0)},mul=function(){for(var n=arguments.length,r=Array(n),t=0;t<n;t++)r[t]=arguments[t];return r.reduce(function(n,r){return _u(n,r)},1)},sub=function(n,r){return n-r},div=function(n,r){return n/r},mod=function(n,r){return n%r},abs=function(n){return _b(n)},pow=function(r,t){return _p(n,m)},sqrt=function(n){return _s(n)};
+
+function main () {
+	let x = add(1, 2, 3, 4, 5, 6, 7, 8);
+	let y = mul(1, 2, 3, 4, 5, 6, 7, 8);
+	print("sum is :", x);
+	print("mul is :", y);
+	$if(gt(x, y), function () {
+		print("sum is bigger than product..!");
+		
+	}, function () {
+		print("sum is less than product..!");
+		
+	});
+	
+};
+main();
